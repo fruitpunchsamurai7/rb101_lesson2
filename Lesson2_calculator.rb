b7f8cbb5260d6434eeafe69ece7387dfad5e42af
kaboom786 <ali.sheikh.toronto@gmail.com>
2019-06-17 11:59:34 -0400

    Initial commit

---
+++ Lesson2_calculator.rb
@@ -0,0 +1,14 @@
+#ask the user for two numbers
+# ask the user for an operation to perform
+# perform the operation on the two numbers
+# output the result
+
+Kernel.puts("Welcome to Calculator")
+
+Kernel.puts("What's the first number?")
+number1=Kernel.gets().chomp()
+
+Kernel.puts("the number is: " + number1+"!")
+puts("ok!")
+puts("Ali")
+puts
