0a8342bfe90869a3cf20eead286ca63f031760a1
kaboom786 <ali.sheikh.toronto@gmail.com>
2019-06-17 12:33:07 -0400

    test two numbers

--- Lesson2_calculator.rb
+++ Lesson2_calculator.rb
@@ -12,3 +12,21 @@ Kernel.puts("What's the second number?")
 number2=Kernel.gets().chomp()
 
 Kernel.puts(number1+" "+number2)
+
+Kernel.puts("what would you like to do? 1) Add 2) Subtract 3) Multiply 4) Divide")
+operator=Kernel.gets().chomp()
+
+if operator =='1'
+  result=number1.to_i+number2.to_i
+elsif
+  operator=='2'
+  result=number1.to_i-number2.to_i
+elsif
+  operator=='3'
+  result=number1.to_i*number2.to_i
+elsif
+  operator=='4'
+  result=number1.to_i/number2.to_i
+end
+
+Kernel.puts("The result is #{result}")
