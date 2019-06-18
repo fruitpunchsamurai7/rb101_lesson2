6e6e05eef2ff96ae2fd79bb767e5e0c82bf41f1f
kaboom786 <ali.sheikh.toronto@gmail.com>
2019-06-18 14:17:53 -0400

    Loops - sub processes

--- Lesson2_calculator.rb
+++ Lesson2_calculator.rb
@@ -5,30 +5,56 @@
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
-
+def valid_number?(num)
+  num.to_i() != 0
+end
 
 prompt("Welcome to calculator")
 
-prompt("What's the first number?")
-number1=Kernel.gets().chomp()
+loop do
+    number1='1'
+    loop do
+      prompt("What's the first number?")
+      number1=Kernel.gets().chomp()
 
-prompt("What's the second number?")
-number2=Kernel.gets().chomp()
+      if valid_number?(number1)
+        break
+      else
+        prompt("Oops, this number is invalid")
+      end
+    end
 
-prompt(number1+" "+number2)
+    number2=''
+    loop do
+      prompt("What's the second number?")
+      number2=Kernel.gets().chomp()
 
-prompt("what would you like to do? 1) Add 2) Subtract 3) Multiply 4) Divide")
-operator=Kernel.gets().chomp()
+      if valid_number?(number2)
+        break
+      else
+        prompt("Oops, this number is invalid")
+      end
+    end
+
+    prompt(number1+" "+number2)
+
+    prompt("what would you like to do? 1) Add 2) Subtract 3) Multiply 4) Divide")
+    operator=Kernel.gets().chomp()
 
 
-result=case operator
-       when '1'
-         number1.to_i()+number2.to_i()
-       when '2'
-         number1.to_i()-number2.to_i()
-       when '3'
-         number1.to_i()*number2.to_i()
-       when '4'
-         number1.to_f()/number2.to_f()
-       end
-prompt("The result is #{result}")
+    result=case operator
+           when '1'
+             number1.to_i()+number2.to_i()
+           when '2'
+             number1.to_i()-number2.to_i()
+           when '3'
+             number1.to_i()*number2.to_i()
+           when '4'
+             number1.to_f()/number2.to_f()
+           end
+    prompt("The result is #{result}")
+    prompt("Do you want to perform another calculation?")
+    answer=gets().chomp()
+    break unless answer.downcase().start_with?('y')
+end
+prompt("Bye!")
